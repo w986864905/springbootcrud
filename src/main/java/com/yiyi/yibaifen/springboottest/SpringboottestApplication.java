0552bdc087355fe2e5a8bf6ade37632e9081b163
王品峰 <wangpinfeng@enn.cn>
2020-07-17 10:32:24 +0800

    springfox3.0整个swagger简单使用

--- src/main/java/com/yiyi/yibaifen/springboottest/SpringboottestApplication.java
+++ src/main/java/com/yiyi/yibaifen/springboottest/SpringboottestApplication.java
@@ -2,7 +2,9 @@ package com.yiyi.yibaifen.springboottest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.oas.annotations.EnableOpenApi;
 
+@EnableOpenApi
 @SpringBootApplication
 public class SpringboottestApplication {
 
